806d4c5af96d4bff51a2f1bb1eac18738740e44c
Дубов Дмитрий <schliffen@mail.ru>
2024-07-31 15:28:44 +0300

    Деплой

--- scanner/src/main/java/com/exchange/scanner/configs/WebConfig.java
+++ scanner/src/main/java/com/exchange/scanner/configs/WebConfig.java
@@ -25,8 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-//        config.setAllowedOrigins(List.of("http://185.198.152.184:3000", "http://185.198.152.184:80"));
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+        config.setAllowedOrigins(List.of(
+                "http://185.198.152.184:3000",
+                "http://185.198.152.184:80",
+                "http://localhost:3000",
+                "http://localhost:3001"
+        ));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
